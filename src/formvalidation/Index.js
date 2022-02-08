f12e2f31b55de83ceb39dd2eefdce6efc06e1d83
Osama-14 <mohdosama42000@gmail.com>
2022-02-08 21:45:58 +0500

    added validation form

---
+++ src/formvalidation/Index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Form from "../formvalidation/Form"
+
+const Index = () => {
+
+  return (
+  
+  <div>
+      <Form />
+
+
+  </div>
+  )
+};
+
+export default Index;
